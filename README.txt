2a6766ffb0791e35f811034a613a548255102d07
Joi Glifberg <joi.glifberg@triggerfish.se>
2017-11-24 15:29:50 +0100

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,29 @@
+=== Edit Links for ACF Relationship Fields ===
+Contributors: circuuz
+Tags: acf, advanced, custom, field, fields, form, repeater, content, relationship, relationship field
+Requires at least: 4.7
+Tested up to: 4.8.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add edit links to the ACF Relationship Field.
+
+== Description ==
+
+Use the Edit Links for ACF Relationship Fields plugin to add edit links to the ACF Relationship Field.
+
+
+== Installation ==
+
+From your WordPress dashboard
+
+1. **Visit** Plugins > Add New
+2. **Search** for "Edit Links for ACF Relationship Fields"
+3. **Activate** Edit Links for ACF Relationship Fields from your Plugins page
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial commit.
---
+++ includes/class-plugin.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace EditLinksACFRelationshipFields;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Plugin
+ *
+ * @since 1.0
+ * @package EditLinksACFRelationshipFields
+ */
+class Plugin {
+
+	public $version = '1.0.0';
+
+	public $edit_link_class = 'edit-link-acf-relationship-fields';
+
+	public function __construct() {
+		add_action( 'init', [ $this, 'init' ] );
+	}
+
+	public function init() {
+
+		// Only support ACF version 5+.
+		if ( ! function_exists( 'acf_get_setting' ) ) {
+			return;
+		}
+
+		add_filter( 'acf/fields/relationship/result', [ $this, 'add_edit_link' ], 10, 3 );
+		add_filter( 'acf/input/admin_head', [ $this, 'output_css' ], 10, 3 );
+		add_filter( 'acf/input/admin_head', [ $this, 'output_js' ], 10, 3 );
+	}
+
+	public function add_edit_link( $title, $post, $field ) {
+		if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+			return $title;
+		}
+
+		$edit_url = get_edit_post_link( $post );
+
+		if ( empty( $edit_url ) ) {
+			return $title;
+		}
+
+		$title .= sprintf(
+			' (<a class="%s" href="%s">%s</a>)',
+			esc_attr( $this->edit_link_class ),
+			esc_url( $edit_url ),
+			esc_html__( 'Edit' )
+		);
+
+		return $title;
+	}
+
+	public function output_css() {
+		?>
+		<style type="text/css">
+			.acf-field-relationship .<?php echo esc_html( $this->edit_link_class ); ?> {
+				-webkit-transition: none;
+				transition: none;
+			}
+
+			.acf-field-relationship .acf-rel-item:not( .disabled ):hover .<?php echo esc_html( $this->edit_link_class ); ?> {
+				color: #fff;
+			}
+		</style>
+		<?php
+	}
+
+	public function output_js() {
+		?>
+		<script type="text/javascript">
+			( function( $ ) {
+				if ( 'function' !== typeof acf.add_action ) {
+					return;
+				}
+
+				var callback = function( element ) {
+					element.on( 'click touchend', '.<?php echo esc_html( $this->edit_link_class ); ?>', function( e ) {
+						e.stopPropagation();
+					});
+				};
+
+				acf.add_action( 'ready_field/type=relationship', callback );
+				acf.add_action( 'append_field/type=relationship', callback );
+
+			} )( jQuery );
+		</script>
+		<?php
+	}
+}
